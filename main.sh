94aeca3f2de1d94a83b9f61fe3c7f59c3ff3d260
Smollejan <69522210+Smollejan@users.noreply.github.com>
2024-04-11 16:43:22 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,8 +5,8 @@
 
 # Kernel
 KERNEL_NAME="MilkKernel"
-KERNEL_GIT="https://github.com/xiaomi-sm6150/android_kernel_xiaomi_sm6150.git"
-KERNEL_BRANCH="lineage-21"
+KERNEL_GIT="https://github.com/PixelOS-Devices/kernel_xiaomi_sm6150.git"
+KERNEL_BRANCH="fourteen"
 
 # KernelSU
 KERNELSU_REPO="tiann/KernelSU"
